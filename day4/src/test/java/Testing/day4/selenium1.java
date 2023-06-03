9ad27f65ad523ff39b304f7a72577b39d0016c0e
727821TUEC045 <115608584+727821TUEC045@users.noreply.github.com>
2023-06-02 21:16:07 -0700

    Add files via upload

---
+++ day4/src/test/java/Testing/day4/selenium1.java
@@ -0,0 +1,33 @@
+package Testing.day4;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+public class selenium1 {
+
+	public static void main(String[] args)  throws InterruptedException
+	{
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver dr = new ChromeDriver();
+		
+		//maximize the page of the browser
+		dr.manage().window().maximize();
+		dr.get("https://demo.opencart.com/index.php?route=account/register&language=en-gb");
+		
+		//Find the element in the website
+		dr.findElement(By.id("input-firstname")).sendKeys("Gomathi");
+		dr.findElement(By.id("input-lastname")).sendKeys("J");
+		dr.findElement(By.id("input-email")).sendKeys("Gomathi@gmail.com");
+		dr.findElement(By.id("input-password")).sendKeys("password");
+		dr.findElement(By.id("input-newsletter-yes")).click();	
+		
+		Thread.sleep(5000);
+		dr.quit();
+		
+
+	}
+
+}
